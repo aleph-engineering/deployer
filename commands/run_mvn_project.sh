5dbb1c0bd26a5f1f6b227316d5a6225d28717ff1
reFucktor <hmvalcarcel@gmail.com>
2016-02-11 14:07:29 -0500

    Fixing bugs on scripts to run maven.

--- commands/run_mvn_project.sh
+++ commands/run_mvn_project.sh
@@ -5,12 +5,13 @@
 
 GIT_REPOSITORY_URL="ssh://zero@zero/var/lib/public/incubator"
 PROJECT_NAME="incubator"
-PROJECT_LOCATION="/opt/incubator"
+PROJECT_LOCATION="/opt/new_incubator"
 REMOTE_NAME="origin"
 BRANCH_NAME="refactoring"
 
 cd $PROJECT_LOCATION
 git pull $REMOTE_NAME $BRANCH_NAME
+git checkout $BRANCH_NAME
 
 mvn clean install
 cd incubator.web
